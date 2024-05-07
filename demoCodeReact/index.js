dbf7a8ba2127cd1efbcdcea727d4f0e7a1fd6d38
Hoang-Cuong <cuong9dhahav@gmail.com>
2024-05-07 18:50:42 +0700

    hello

--- demoCodeReact/index.js
+++ demoCodeReact/index.js
@@ -5,11 +5,10 @@ const http = require('http');
 const port = 3000;
 const host = '127.0.0.1';
 const server = http.createServer((req, res) => {
-    res.end('fullname: Hoang Tuan Cuong');
-    res.end('class: 17D08');
-    res.end('School: ');
+    res.end('fullname: Hoang Tuan Cuong\nclass: 17D08\nSchool: FPT university\nDesctiption: friendly, kind');
 });
 
 server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`);
 });
+
